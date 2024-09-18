e9d1fdcb40e254a8874a0e9dbf8e5b004f1f2d60
Boomika <boomika@BoomikaEzhilarasan.local>
2024-09-18 23:25:33 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Replace with your MongoDB credentials
+client = MongoClient("mongodb+srv://kavi22021ad:Kaviya1234@cluster0.jyx09.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client['Constitution']
+collection = db['articles']
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query', '')
+    
+    aggregate_query = [
+    {
+        "$search": {
+            "text": {
+                "query": query,
+                "path": ["content", "title", "topic"]
+            },
+            "highlight": {
+                "path": ["content", "title", "topic"]
+            }
+        }
+    },
+    {
+        "$project": {
+            "article_number": 1,
+            "title": 1,
+            "content": 1,
+            "topic": 1,
+            "score": {"$meta": "searchScore"},
+            "highlights": {"$meta": "searchHighlights"}
+        }
+    }
+]
+
+    
+    results = list(collection.aggregate(aggregate_query))
+    
+    response = []
+    for result in results:
+        response.append({
+            "topic":result.get("topic","No topic"),
+            "title": result.get("title", "No Title"),
+            "content": result.get("content", "No Content"),
+            "highlights": result.get("highlights", [])
+        })
+    
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
