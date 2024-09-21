72ad124d2920c422ba41d0bcccb18746d623ef38
Boomika <boomika@BoomikaEzhilarasan.local>
2024-09-21 19:28:10 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, redirect
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -10,43 +10,42 @@ collection = db['articles']
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return redirect("https://app-offa.vercel.app/", code=302)
 
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('query', '')
     
     aggregate_query = [
-    {
-        "$search": {
-            "text": {
-                "query": query,
-                "path": ["content", "title", "topic"]
-            },
-            "highlight": {
-                "path": ["content", "title", "topic"]
+        {
+            "$search": {
+                "text": {
+                    "query": query,
+                    "path": ["content", "title", "topic"]
+                },
+                "highlight": {
+                    "path": ["content", "title", "topic"]
+                }
+            }
+        },
+        {
+            "$project": {
+                "article_number": 1,
+                "title": 1,
+                "content": 1,
+                "topic": 1,
+                "score": {"$meta": "searchScore"},
+                "highlights": {"$meta": "searchHighlights"}
             }
         }
-    },
-    {
-        "$project": {
-            "article_number": 1,
-            "title": 1,
-            "content": 1,
-            "topic": 1,
-            "score": {"$meta": "searchScore"},
-            "highlights": {"$meta": "searchHighlights"}
-        }
-    }
-]
+    ]
 
-    
     results = list(collection.aggregate(aggregate_query))
     
     response = []
     for result in results:
         response.append({
-            "topic":result.get("topic","No topic"),
+            "topic": result.get("topic", "No topic"),
             "title": result.get("title", "No Title"),
             "content": result.get("content", "No Content"),
             "highlights": result.get("highlights", [])
